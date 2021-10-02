15dfbe04bcd1be4fa3e6f0f250493a7fe9416c2c
Michael Vetkin <mvetkin@freetechs.org>
2021-10-02 21:04:48 +0300

    init MVP

---
+++ index.js
@@ -0,0 +1,54 @@
+function getListOfElementsWithAttribute(attributeName) {
+  const foundElements = document.querySelectorAll(`[${attributeName}]`);
+  const foundElementsList = Array.from(foundElements);
+
+  return foundElementsList;
+}
+
+function highlightElements(elements) {
+  elements.forEach(element => {
+    element.classList.add('highlighted-element');
+
+    element.addEventListener('mousemove', (event) => {
+      var x = event.clientX;
+      var y = event.clientY;
+
+      const tipWrapper = document.getElementById('tooltip');
+
+      tipWrapper.innerText = element.getAttribute('data-tnav');
+
+      tipWrapper.style.left = `${x + 16}px`;
+      tipWrapper.style.top = `${y + 16}px`;
+      tipWrapper.style.display = 'block';
+      tipWrapper.style.visibility = 'visible';
+
+      if (event.target === element) {
+        event.stopPropagation();
+      }
+    });
+
+    element.addEventListener('mouseleave', () => {
+      const tipWrapper = document.getElementById('tooltip');
+      tipWrapper.style.display = 'none';
+      tipWrapper.style.visibility = 'hidden';
+      tipWrapper.style.left = '0px';
+      tipWrapper.style.top = '0px';
+    })
+  });
+}
+
+function addTooltipToDocument() {
+  const tipWrapper = document.createElement('div');
+
+  tipWrapper.className = 'tooltip';
+  tipWrapper.id = 'tooltip';
+
+  document.body.appendChild(tipWrapper);
+}
+
+const ATTRIBUTE_NAME = 'data-tnav';
+
+const foundElementsList = getListOfElementsWithAttribute(ATTRIBUTE_NAME);
+highlightElements(foundElementsList);
+addTooltipToDocument();
+
