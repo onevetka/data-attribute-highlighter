df322798c0ad9044b3a8889a8cbd3917f49630be
Michael Vetkin <vetkin@veren.team>
2021-10-14 00:09:01 +0300

    Added React and TypeScript

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,3 +1,10 @@
+// import { printLine } from './modules/print';
+
+// console.log('Content script works!');
+// console.log('Must reload extension for modifications to take effect.');
+
+// printLine("Using the 'printLine' function from the Print Module");
+
 function getListOfElementsWithAttribute(attributeName) {
   const foundElements = document.querySelectorAll(`[${attributeName}]`);
   const foundElementsList = Array.from(foundElements);
@@ -58,3 +65,4 @@ document.addEventListener('click', () => {
     highlightElements(fElementsList);
   }, 100);
 });
+
