3c13212e11c55986a9706a6ab77239ed110c4c17
Michael Vetkin <vetkin@veren.team>
2021-10-30 20:02:45 +0300

    Deleted excess files and added stories for input

---
+++ src/components/Input/Input.stories.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import Input, { InputStatus } from './index';
+
+export default {
+  title: 'Input',
+  component: Input,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+    status: {
+      options: { ...InputStatus, unset: undefined },
+      control: { type: 'select' },
+    },
+  },
+} as ComponentMeta<typeof Input>;
+
+const Template: ComponentStory<typeof Input> = ({ value, onChange, ...args }) => {
+  const [inputValue, setValue] = useState('');
+
+  return <Input onChange={setValue} value={inputValue} {...args} />;
+};
+
+export const Default = Template.bind({});
+Default.args = {
+  disabled: false,
+  value: 'Input',
+  statusText: '',
+  placeholder: 'Placeholder',
+};
