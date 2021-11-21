9e80c76c4a4fc4fc6c2a58febaabf94df05ae4ab
Michael Vetkin <vetkin@veren.team>
2021-11-22 01:49:05 +0300

    Refactoring: Created Shadow service

--- src/pages/Content/modules/highlighter.ts
+++ src/pages/Content/modules/highlighter.ts
@@ -10,6 +10,94 @@ type Selector = {
   tooltip: Tooltip;
 };
 
+class ShadowService {
+  public static create(color: string) {
+    const shadow = new Shadow();
+
+    shadow.color = color;
+    shadow.spreadRadius = 5;
+
+    return [shadow];
+  }
+
+  public static add(shadows: Array<Shadow>, color: string) {
+    const shadow = new Shadow();
+
+    shadow.color = color;
+    shadow.spreadRadius = (shadows.length + 1) * 5;
+
+    return [...shadows, shadow];
+  }
+
+  public static remove(originalShadows: Array<Shadow>, color: string) {
+    const toRemoveIndex = originalShadows.findIndex(shadow => shadow.color === color);
+
+    if (toRemoveIndex === -1) throw new Error("The element haven't this color");
+
+    const shadows = this.recomputeBordersWidth([
+      ...originalShadows.slice(0, toRemoveIndex),
+      ...originalShadows.slice(toRemoveIndex + 1)
+    ]);
+
+    return shadows;
+  }
+
+  public static shadowArrayToCSS(shadows: Array<Shadow>) {
+    return shadows.map((shadow) => shadow.computeCSS()).join(", ");
+  }
+
+  private static recomputeBordersWidth(shadows: Array<Shadow>) {
+    return shadows.map((shadow, index) => {
+      shadow.spreadRadius = (index + 1) * 5;
+      return shadow;
+    });
+  }
+}
+
+class TooltipDumpService {
+  public static create(element: HTMLElement, color: string, attributeValue: string) {
+    const tooltip = new Tooltip();
+
+    const attribute = {
+      label: attributeValue,
+      color,
+    };
+
+    const attributes = [attribute];
+
+    tooltip.element = element;
+    tooltip.attributes = attributes;
+
+    return tooltip;
+  }
+
+  public static add(tooltip: Tooltip, element: HTMLElement, color: string, attributeValue: string) {
+    // !!! Mutable data
+    const attribute = {
+      label: attributeValue,
+      color,
+    };
+
+    tooltip.attributes = [...tooltip.attributes, attribute];
+
+    return tooltip;
+  }
+
+  public static remove(tooltip: Tooltip, element: HTMLElement, color: string) {
+    const toRemoveIndex = tooltip.attributes.findIndex(attribute => attribute.color === color);
+    const originalAttributes = tooltip.attributes;
+
+    const attributes = [
+      ...originalAttributes.slice(0, toRemoveIndex),
+      ...originalAttributes.slice(toRemoveIndex + 1)
+    ];
+
+    tooltip.attributes = attributes;
+
+    return tooltip;
+  }
+}
+
 class Highlighter {
   public selectedElements: Record<string, Selector>;
 
@@ -21,36 +109,24 @@ class Highlighter {
     const hasSelector = Boolean(element.dataset.highlightedElemId);
     const id = element.dataset.highlightedElemId || getHash();
 
-    const shadows = hasSelector ? this.selectedElements[id].shadows : [];
-    const spreadSize = hasSelector ? (shadows.length + 1) * 5 : 5;
+    const originalShadow = this.selectedElements[id]?.shadows;
+    const originalTooltip = this.selectedElements[id]?.tooltip;
+
+    const shadows = hasSelector
+      ? ShadowService.add(originalShadow, color)
+      : ShadowService.create(color);
 
     const tooltip = hasSelector
-      ? this.selectedElements[id].tooltip
-      : new Tooltip();
-    const attributes = hasSelector ? tooltip.attributes : [];
-
-    const shadow = new Shadow();
-
-    shadow.color = color;
-    shadow.spreadRadius = spreadSize;
-
-    const attribute = {
-      label: attributeValue,
-      color
-    };
-
-    tooltip.attributes = [...attributes, attribute];
-    tooltip.element = element;
+      ? TooltipDumpService.add(originalTooltip, element, color, attributeValue)
+      : TooltipDumpService.create(element, color, attributeValue);
 
     this.selectedElements[id] = {
-      shadows: [...shadows, shadow],
+      shadows,
       tooltip
     };
 
     element.dataset.highlightedElemId = id;
-    element.style.boxShadow = this.getBoxShadow(
-      this.selectedElements[id].shadows
-    );
+    element.style.boxShadow = ShadowService.shadowArrayToCSS(shadows);
     TooltipService.addToElement(element, tooltip);
   }
 
@@ -60,43 +136,21 @@ class Highlighter {
     if (!id) throw new Error("This element has not highlighted");
 
     const originalShadows = this.selectedElements[id].shadows;
-    const originalAttributes = this.selectedElements[id].tooltip.attributes;
+    const originalTooltip = this.selectedElements[id].tooltip;
 
-    const toRemoveIndex = originalShadows.findIndex(
-      (shadow) => shadow.color === color
-    );
+    const shadows = ShadowService.remove(originalShadows, color);
+    const tooltip = TooltipDumpService.remove(originalTooltip, element, color);
 
-    if (toRemoveIndex === -1) throw new Error("The element haven't this color");
-
-    const shadows = this.recomputeBordersWidth([
-      ...originalShadows.slice(0, toRemoveIndex),
-      ...originalShadows.slice(toRemoveIndex + 1)
-    ]);
-
-    const attributes = [
-      ...originalAttributes.slice(0, toRemoveIndex),
-      ...originalAttributes.slice(toRemoveIndex + 1)
-    ];
-
-    element.style.boxShadow = this.getBoxShadow(shadows);
+    this.selectedElements[id] = {
+      shadows,
+      tooltip,
+    }
 
     if (shadows.length === 0) {
       delete element.dataset.highlightedElemId;
     }
 
-    this.selectedElements[id].shadows = shadows;
-    this.selectedElements[id].tooltip.attributes = attributes;
-  }
-
-  private getBoxShadow(shadows: Array<Shadow>) {
-    return shadows.map((shadow) => shadow.computeCSS()).join(", ");
-  }
-
-  private recomputeBordersWidth(shadows: Array<Shadow>) {
-    return shadows.map((shadow, index) => {
-      shadow.spreadRadius = (index + 1) * 5;
-      return shadow;
-    });
+    element.style.boxShadow = ShadowService.shadowArrayToCSS(shadows);
   }
 }
 
