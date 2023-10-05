154d06ae5b60bc62de2033e70acdc86f068accc3
Michael Vetkin <onevetka@gmail.com>
2023-10-06 02:20:23 +0300

    Fixed key error

--- src/feature/attributeList/ui/components/ListSkeleton/index.tsx
+++ src/feature/attributeList/ui/components/ListSkeleton/index.tsx
@@ -8,7 +8,11 @@ const ListSkeleton: React.FC = () => {
   return (
     <div className={styles.skeletonWrapper}>
       {[1.0, 0.7, 0.4].map((item) => (
-        <div className={styles.skeletonItem} style={{ opacity: `${item}` }} />
+        <div
+          className={styles.skeletonItem}
+          style={{ opacity: `${item}` }}
+          key={item}
+        />
       ))}
       <div className={styles.label}>No highlighted attributes</div>
     </div>
